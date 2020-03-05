85ca8b20631f806d346ccb52769547c5d3de97e0
Doroszlai, Attila <adoroszlai@apache.org>
2020-03-05 13:45:17 +0100

    reduce number of datanodes/pipelines

--- hadoop-ozone/integration-test/src/test/java/org/apache/hadoop/ozone/freon/TestDataValidate.java
+++ hadoop-ozone/integration-test/src/test/java/org/apache/hadoop/ozone/freon/TestDataValidate.java
@@ -33,7 +33,7 @@ public abstract class TestDataValidate {
 
   static void startCluster(OzoneConfiguration conf) throws Exception {
     cluster = MiniOzoneCluster.newBuilder(conf)
-        .setNumDatanodes(5).setTotalPipelineNumLimit(8).build();
+        .setNumDatanodes(3).build();
     cluster.waitForClusterToBeReady();
   }
 
