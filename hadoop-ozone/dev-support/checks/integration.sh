d1b5f831f0e2d0d32dc0310ac67a066743966c44
Doroszlai, Attila <adoroszlai@apache.org>
2020-04-22 13:55:00 +0200

    TEST 50x TestDnRatisLogParser,TestOMRatisLogParser

--- hadoop-ozone/dev-support/checks/integration.sh
+++ hadoop-ozone/dev-support/checks/integration.sh
@@ -24,14 +24,26 @@ mkdir -p "$REPORT_DIR"
 
 export MAVEN_OPTS="-Xmx4096m"
 mvn -B install -DskipTests
-mvn -B -fae test -pl :hadoop-ozone-integration-test,:mini-chaos-tests "$@" \
-  | tee "${REPORT_DIR}/output.log"
-rc=$?
 
-# shellcheck source=hadoop-ozone/dev-support/checks/_mvn_unit_report.sh
-source "$DIR/_mvn_unit_report.sh"
+rc=0
+for i in {1..50}; do
+  original_report_dir="${REPORT_DIR}"
+  REPORT_DIR="${original_report_dir}/iteration${i}"
+  mkdir -p "${REPORT_DIR}"
 
-if [[ -s "$REPORT_DIR/summary.txt" ]] ; then
-    exit 1
-fi
+  mvn -B -fae test -pl :hadoop-ozone-integration-test "$@" \
+    | tee "${REPORT_DIR}/output.log"
+  irc=$?
+
+  # shellcheck source=hadoop-ozone/dev-support/checks/_mvn_unit_report.sh
+  source "${DIR}/_mvn_unit_report.sh"
+
+  REPORT_DIR="${original_report_dir}"
+  echo "Iteration ${i} exit code: ${irc}" | tee -a "${REPORT_DIR}/output.log"
+
+  if [[ ${rc} == 0 ]]; then
+    rc=${irc}
+  fi
+done
+
 exit ${rc}
