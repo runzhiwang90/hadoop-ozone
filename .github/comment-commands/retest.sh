98f4b6a5290795a6b691de7b25710e28c6e012f8
Doroszlai, Attila <adoroszlai@apache.org>
2020-06-27 19:54:13 +0200

    TEST - contributor

--- .github/comment-commands/retest.sh
+++ .github/comment-commands/retest.sh
@@ -24,7 +24,7 @@ set -eu
 code='```'
 
 pr_url="$(jq -r '.issue.pull_request.url' "${GITHUB_EVENT_PATH}")"
-commenter="$(jq -r '.comment.user.login' "${GITHUB_EVENT_PATH}")"
+commenter=asdf # "$(jq -r '.comment.user.login' "${GITHUB_EVENT_PATH}")"
 assoc="$(jq -r '.comment.author_association' "${GITHUB_EVENT_PATH}")"
 
 curl -LSs "${pr_url}" -o pull.tmp
