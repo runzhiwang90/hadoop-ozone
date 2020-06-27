e0397d69fc90064e54fa03fc322e2fcfbc25eba8
Doroszlai, Attila <adoroszlai@apache.org>
2020-06-27 20:05:21 +0200

    test maint mod

--- .github/comment-commands/retest.sh
+++ .github/comment-commands/retest.sh
@@ -31,7 +31,7 @@ curl -LSs "${pr_url}" -o pull.tmp
 source_repo="$(jq -r '.head.repo.ssh_url' pull.tmp)"
 branch="$(jq -r '.head.ref' pull.tmp)"
 pr_owner="$(jq -r '.head.user.login' pull.tmp)"
-maintainer_can_modify="$(jq -r '.maintainer_can_modify' pull.tmp)"
+maintainer_can_modify=true # "$(jq -r '.maintainer_can_modify' pull.tmp)"
 
 if [[ "${commenter}" == "${pr_owner}" ]]; then
   cat <<-EOF
